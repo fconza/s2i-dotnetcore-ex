b0a6f2dd048b201c5093101c1e7286c801ae5cae
fconza <francois.Conza@free.fr>
2018-12-01 14:59:13 +0100

    Change row

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace app.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "First test of .NET core application.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "How to contact me.";
 
             return View();
         }
