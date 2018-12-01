87c30bfaaa029c7840b44990c5c28841ff50beb3
fconza <francois.Conza@free.fr>
2018-12-01 18:16:56 +0100

    Add app Controller

---
+++ app/Controllers/App1Controller.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class App1Controller : Controller
+    {
+
+        public IActionResult Application1()
+        {
+            ViewData["Message"] = "Test of First Application.";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ app/Controllers/App2Controller.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class App2Controller : Controller
+    {
+
+        public IActionResult Application2()
+        {
+            ViewData["Message"] = "Test of Second Application.";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
